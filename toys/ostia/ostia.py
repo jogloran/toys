59b728d26566c4bc8225aa634d47b9f68b84dd66
daniel.tse <daniel@semanaut.com>
2012-03-05 22:26:45 +1100

    still broken

--- toys/ostia/ostia.py
+++ toys/ostia/ostia.py
@@ -20,7 +20,7 @@ from collections import defaultdict
 
 class ddict(defaultdict):
     def __repr__(self):
-        s = '{'
+        s = '{ '
         for k, v in self.iteritems():
             s += '%s: %s ' % (k,v)
         s += '}'
@@ -36,6 +36,15 @@ class State(object):
     def __repr__(self):
         return '<St %s:%s>' % (self.label, self.output)
         
+    def __hash__(self):
+        return hash(self.label)
+        
+    def __eq__(self, other):
+        return self.label == other.label
+        
+    def __deepcopy__(self, memo):
+        return self#State(self.label, self.output)
+        
 LAMBDA = ''
 class Transducer(object):
     # { state: { input: [output, state] } }
@@ -45,8 +54,8 @@ class Transducer(object):
         self.states = {}
         
     def incoming_states_to(self, target):
-        for state in self.incoming[target]:
-            yield state
+        for (state, input) in self.incoming[target]:
+            yield (state, input)
         
     def get_state(self, state_label):
         if state_label not in self.states:
@@ -61,7 +70,7 @@ class Transducer(object):
     def add_transition(self, source, input, output, target):
         self.T[source][input] = [output, target]
         
-        self.incoming[target].add(source)
+        self.incoming[target].add( (source, input) )
         
     def edge_output_string(self, source, input):
         return self.T[source][input][0]
@@ -73,14 +82,14 @@ class Transducer(object):
     def set_edge_target_state(self, source, input, target):    
         self.T[source][input][1] = target
         
-        self.incoming[target].add(source)
+        self.incoming[target].add( (source, input) )
         
     def outgoing_states_from(self, source):
-        for (input, (output, target)) in self.T[source].iteritems():
+        for (input, (output, target)) in self.T[source].items():
             yield target
             
     def outgoing_edges_from(self, source):
-        for (input, (output, target)) in self.T[source].iteritems():
+        for (input, (output, target)) in self.T[source].items():
             yield (input, output, target)
             
     def all_states(self):
@@ -88,7 +97,7 @@ class Transducer(object):
             yield state
             
     def __repr__(self):
-        return '<Transducer states=%s transitions=%s>' % (self.states.values(), self.T)
+        return '<Transducer %s>' % self.T
             
 def flatten(lol):
     '''
@@ -151,17 +160,15 @@ def lcp(files):
 '123'
 >>> lcp(['123', '12', BOT])
 '12'
->>> lcp([BOT, BOT])
-BOT
-    '''
-    if (all( f == BOT for f in files )):
-        return BOT
-        
-    # treat BOT as nothing
-    files = [ f for f in files if f != BOT ]
-    
+'''    
     if len(files) == 0:
         return ''
+        
+    if (all( f == BOT for f in files )):
+        return BOT
+
+    # treat BOT as nothing
+    files = [ f for f in files if f != BOT ]
         
     if len(files) == 1:
         return files[0]
@@ -194,16 +201,12 @@ def make_onward(T, state):
         f = state.output
     len_f = len(f)
     
-    print 'state %s, lcp: %s' % (state, f)
-    
     if len_f > 0:
         for (input, output, outgoing_state) in T.outgoing_edges_from(state):
             T.set_edge_output_string(state, input,
                                      T.edge_output_string(state, input)[len_f:])
 
-        if state.output == BOT: 
-            state.output = BOT
-        else:
+        if state.output is not BOT:
             state.output = state.output[len_f:]
         
     return f
@@ -233,20 +236,42 @@ def pushback(T, state1, state2, input):
     for (input, output, outgoing_state) in T.outgoing_edges_from(state2_successor):
         T.set_edge_output_string(state2_successor, input,
                                  u2 + T.edge_output_string(state2_successor, input))
-    
-    state1_successor.output = u1 + state1_successor.output
-    state2_successor.output = u2 + state2_successor.output
+
+    if state1_successor.output is not BOT:
+        state1_successor.output = u1 + state1_successor.output
+    if state2_successor.output is not BOT:
+        state2_successor.output = u2 + state2_successor.output
     
 def merge(orig_T, red_states, red_state, blue_state):
     # for any state incoming to blue_state:
     #     point it to red_state instead
     # fold(T, red_state, blue_state)
-    T = copy.deepcopy(orig_T)
-    for incoming_state in T.incoming_states_to(blue_state):
-        for (input, output, outgoing_state) in T.outgoing_edges_from(incoming_state):
-            T.set_edge_target_state(input, output, red_state)
+    T = orig_T#copy.deepcopy(orig_T)
     
-    return fold(T, red_states, red_state, blue_state)
+    # red_state = T.get_state(red_state.label)
+    # blue_state = T.get_state(blue_state.label)
+    
+    reassignments = {}
+    # for every state that used to point to blue_state,
+    for incoming_state, input in T.incoming_states_to(blue_state):
+        # point it to red_state instead
+        
+        previous_target_state = T.edge_target_state(incoming_state, input)
+        print 'setting destination of %s on %s from %s to %s' % (
+            incoming_state, input, previous_target_state, red_state)
+        reassignments[ (incoming_state, input) ] = previous_target_state
+        T.set_edge_target_state(incoming_state, input, red_state)
+
+    result = fold(T, red_states, red_state, blue_state)
+
+    if result is None:
+        # roll back
+        for (incoming_state, input), old_target_state in reassignments.items():
+            print 'rolling back destination of %s on %s from %s to %s' % (
+                incoming_state, input, T.edge_target_state(incoming_state, input), old_target_state)
+            T.set_edge_target_state(incoming_state, input, old_target_state)
+            
+    return result
     
 def fold(T, red_states, q, q_):
     w = outputs_are_equal(q.output, q_.output)
@@ -254,75 +279,72 @@ def fold(T, red_states, q, q_):
     if w is None:
         return None
     else:
+        old_q_output = q.output
         q.output = w
 
-        for a in common_input_strings_out_of_states(T, q, q_):        
-            if T.edge_target_state(q_, a) is not None:
-                if T.edge_target_state(q, a) is not None:
-                    # if q on input a takes you to a red state, fail
-                    if T.edge_target_state(q, a) in red_states:
+        for (input_, output_, outgoing_state_) in T.outgoing_edges_from(q_):
+            found_matching = False
+            for (input, output, outgoing_state) in T.outgoing_edges_from(q):
+                if input == input_:
+                    if T.edge_target_state(q, input) in red_states:
+                    # if T.edge_output_string(q, input) != T.edge_output_string(q_, input):
+                        # import pdb;pdb.set_trace()
+                        print '%s is a red state, failing' % T.edge_target_state(q, input)
+                        q.output = old_q_output
                         return None
                     else:
-                        pushback(T, q, q_, a)
-                        fold(T, red_states, T.edge_target_state(q, a), T.edge_target_state(q_, a))
-                else:
-                    T.set_edge_target_state(q, a,
-                                            T.edge_target_state(q_, a))
-
-        # T.remove_state(q_.label)
-                
+                        pushback(T, q, q_, input)
+                        fold(T, red_states, T.edge_target_state(q, input), T.edge_target_state(q_, input))
+                        found_matching = True
+            
+            if not found_matching:
+                T.add_transition(q, input_,
+                                 T.edge_output_string(q_, input_),
+                                 T.edge_target_state(q_, input_))                
         return T
-        # for any input string A on an edge coming out of both q and q_,
-        #     if output strings are not equal, fail
-        #     else,
-        #         pushback(T, q, q_, A)
-        #         fold(T, destination state of q on A, destination state of q_ on A)
-        #    TODO: need to handle 'else: t(q,a)<-t(q',a)' line
-        
-def common_input_strings_out_of_states(T, state1, state2):
-    state1_inputs = set()
-    state2_inputs = set()
-    
-    for (input, _, _) in T.outgoing_edges_from(state1):
-        state1_inputs.add(input)
-    for (input, _, _) in T.outgoing_edges_from(state2):
-        state2_inputs.add(input)
-        
-    # intersection
-    return state1_inputs & state2_inputs
         
 def ostia(data):
     T = build_ptt(data)
-    print 'after build_ptt:\n',T
+    print 'original'
+    print T
     make_onward(T, T.get_state(''))
-    print 'after make_onward:\n',T
+    print 'onward'
+    print T
     
     red_states = { T.get_state('') }
-    blue_states = { T.get_state(input) for (input, output) in data }
+    blue_states = [ T.get_state(input) for (input, output) in data if len(input) == 1 ]
 
     while blue_states:
-        q = blue_states.pop()
-        print 'blue state: %s' % q
+        print 'blue states: ',blue_states
+        print 'red states: ',red_states
+        
+        q = blue_states.pop(0)
+        print 'POP blue state: %s' % q
         
         for p in red_states:
-            print 'trying to merge %s and %s' % (p,q)
+            print 'TRYING MERGE %s and %s' % (p,q)
             merged_T = merge(T, red_states, p, q)
             if merged_T is not None:
                 # accept merged as the new T
-                print 'accepting merged_T %s' % merged_T
+                print 'SUCCEEDED MERGE %s and %s merged_T %s' % (p, q, merged_T)
                 T = merged_T
                 break
             else:
-                print 'failed to merge'
+                print 'FAILED MERGE %s and %s' % (p,q)
         else:
+            print 'exhausted all merges between %s and red states' % q
             red_states.add(q)
             
         # update blue_states to contain
         # any state which a red state points to, but is not red
         for red_state in red_states:
+            # print 'considering red state %s' % red_state
+            
             for (input, output, outgoing_state) in T.outgoing_edges_from(red_state):
+                # if outgoing_state is None: continue
                 if outgoing_state not in red_states:
-                    blue_states.add(outgoing_state)
+                    if outgoing_state not in blue_states: # to get set behaviour
+                        blue_states.append(outgoing_state)
                     
     return T
 
@@ -334,19 +356,65 @@ if __name__ == '__main__':
         doctest.testmod()
         
     else:
-        #data = [ ('a', '1'), ('b', '1'), ('aa', '01'), ('aaa', '001'), ('abab', '0101') ]
-        # T = ostia(data)
+        data = [ ('a', '1'), ('b', '1'), ('aa', '01'), ('ab', '01'), ('aaa', '001'), ('abab', '0101') ]
+        ostia(data)
+        # T = build_ptt(data)
+        # make_onward(T, T.get_state(''))
         # print T
+        # red_states = [T.get_state('')]
+        # print
+        # _ = merge(T, red_states, T.get_state(''), T.get_state('a'))
+        # print 'new:', _
+        # T = merge(T, red_states, T.get_state(''), T.get_state('b'))
+        # print 'new:', T
+        # T = merge(T, red_states, T.get_state('a'), T.get_state('aa'))
+        # print 'new:', T
+        # sys.exit(1)
         
-        T = Transducer()
-        q1, q2, q3, q4, q5, q6 = State('q1', '0'), State('q2', '1'), \
-            State('q3', '0'), State('q4', LAMBDA), \
-            State('q5', LAMBDA), State('q6', '1')
-        T.add_transition(q1, 'a', '1', q3)
-        T.add_transition(q3, 'b', '1', q5)
-        T.add_transition(q2, 'a', '10', q4)
-        T.add_transition(q4, 'c', '1', q6)
-        print T
-        pushback(T, q1, q2, 'a')
-        print T
+        # fold tests
+        # T = Transducer()
+        #         q_ = State('q_', '0')
+        #         qa = State('qa', '1')
+        #         qb = State('qb', '0')
+        #         qaa = State('qaa', '0')
+        #         qaaa = State('qaaa', LAMBDA)
+        #         qaab = State('qaab', BOT)
+        #         qaabb = State('qaabb', LAMBDA)
+        #         T.add_transition(q_, 'b', '1', qb)
+        #         T.add_transition(qb, 'a', '1', q_)
+        #         T.add_transition(q_, 'a', '1', qa)
+        #         T.add_transition(qa, 'a', '1', q_)
+        #         T.add_transition(qa, 'b', LAMBDA, qb)
+        #         
+        #         T.add_transition(qaab, 'b', LAMBDA, qaabb)
+        #         T.add_transition(qaa, 'b', '11', qaab)
+        #         T.add_transition(qaa, 'a', '11', qaaa)
+        #         fold(T, [q_], q_, qaa) # expecting figure 18.10
+        #         fold(T, [q_, qaa], q_, qaaa)
+        #         print T
+        
+        # make_onward tests with BOT
+        # T = Transducer()
+        # q_ = State('q_', '1')
+        # qa = State('qa', BOT)
+        # qaa = State('qaa', '1')
+        # qab = State('qab', LAMBDA)
+        # T.add_transition(q_, 'a', '0', qa)
+        # T.add_transition(qa, 'a', '00', qaa)
+        # T.add_transition(qa, 'b', LAMBDA, qab)
+        # T.add_transition(qaa, 'a', LAMBDA, qab)
+        # make_onward(T, q_)
+        
+        # pushback tests
+        # T = Transducer()
+        # q1, q2, q3, q4, q5, q6 = State('q1', '0'), State('q2', '1'), \
+        #     State('q3', '0'), State('q4', LAMBDA), \
+        #     State('q5', LAMBDA), State('q6', '1')
+        # T.add_transition(q1, 'a', '1', q3)
+        # T.add_transition(q3, 'b', '1', q5)
+        # T.add_transition(q2, 'a', '10', q4)
+        # T.add_transition(q4, 'c', '1', q6)
+        # print T
+        # pushback(T, q1, q2, 'a')
+        # print T
         
